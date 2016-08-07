95add6f1c170cfd3846e30feacc1b85d7e495e24
cycorld <cycorld.com@gmail.com>
2016-08-07 10:56:50 +0900

    UserMailer bugfix

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,8 +1,8 @@
-class UserMailer < ApplicationMailer  
+class UserMailer < ApplicationMailer
   def answer_notification(question, user)
     @question = question
     @user = user
-    @url = "http://unlock-n-ror.herokuapp.com/question/" + @question.id
+    @url = "http://unlock-n-ror.herokuapp.com/question/" + @question.id.to_s
     mail(to: @user.email, subject: "Someone answered to your question!")
     puts "Sending mail"
   end
@@ -10,7 +10,7 @@ class UserMailer < ApplicationMailer
   def comment_notification(commentable, user)
     @commentable = commentable
     @user = user
-    @url = "http://unlock-n-ror.herokuapp.com/question/" + get_question_id(@commentable)
+    @url = "http://unlock-n-ror.herokuapp.com/question/" + get_question_id(@commentable).to_s
     mail(to: @user.email, subject: "Someone commented to your question/answer!")
   end
 
