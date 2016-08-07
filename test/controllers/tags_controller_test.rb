391168644a138f29ecdfe3ac927bbaf09b8b3719
malkoG <rijgndqw012@gmail.com>
2016-08-08 02:16:47 +0900

    Cleanup

--- test/controllers/tags_controller_test.rb
+++ test/controllers/tags_controller_test.rb
@@ -1,12 +1,5 @@
 require 'test_helper'
 
 class TagsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    assert_response :success
-  end
-
-  test "should get show" do
-    assert_response :success
-  end
 
 end
